eed9857722bfbb1f5a87d8a6b17cc2fa0eb79d24
DhanrajSingh18 <db5000918@gmail.com>
2024-07-25 19:04:20 +0530

    Add files via upload

---
+++ pr1/d3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+int a1,a2,a3;
+
+
+
+printf("Enter the a1:");
+scanf("%d",&a1);
+printf("Enter the a2:");
+scanf("%d",&a2);
+a3=180-(a1+a2);
+printf("ans=%d",a3);
+
+
+
+
+}
---
+++ pr1/dhanraj.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float c,f;
+
+printf("Enter the valu of c:");
+scanf("%f",&c);
+
+
+f=1.8*c+32;
+
+printf("f = %f",f);
+
+
+}
