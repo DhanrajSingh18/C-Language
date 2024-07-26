d097fc5255f5c5de9fe0e388f09830973ef32813
DhanrajSingh18 <db5000918@gmail.com>
2024-07-26 18:39:47 +0530

    Add files via upload

---
+++ with,without,witout-,+/drj2.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("enter a:");
+	scanf("%d",&b);
+	 
+	printf("enter b:");
+	scanf("%d",&a);
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	printf("a=%d\n",a);
+	 	printf("b=%d",b);
+	
+	
+	
+}
